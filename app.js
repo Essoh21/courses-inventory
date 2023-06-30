d6b8e705650ac4d4a941d70ca79d27cf8db18f73
essohanam <essohanam.alou@imsp-uac.org>
2023-06-30 08:06:57 +0000

    setup for deployment

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+//production requirements
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+//
 
 const appRouter = require("./routes/allRoutes");
 const { error } = require("console");
@@ -15,9 +20,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 //connetion to mongDb
-const devMongodbURI =
-  "mongodb+srv://essohalou:dbConnectionC@node-blog.ccomzyl.mongodb.net/?retryWrites=true&w=majority";
-
+const devMongodbURI = null; //connection string
 const mongodbURI = process.env.MONGODB_URI || devMongodbURI;
 main().catch((error) => console.log(error));
 async function main() {
@@ -25,13 +28,22 @@ async function main() {
   console.log("successfull connection to db");
 }
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+app.use(helmet());
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// compression on production
+app.use(compression());
+//app.use("/", indexRouter);
+
 app.use(express.static(path.join(__dirname, "public")));
 
-//app.use("/", indexRouter);
 app.use("/", appRouter);
 
 // catch 404 and forward to error handler
